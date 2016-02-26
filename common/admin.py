60c51f5e02911016e8fdb404abbfaf3ca77afe51
Josh Turmel <jturmel@gmail.com>
2016-02-26 11:00:17 -0600

    Add a NotAddableAdminMixin
    * Also rename NotDeletableAdmin to NotDeletableAdminMixin and remove subclassing

--- common/admin.py
+++ common/admin.py
@@ -1,12 +1,17 @@
-from django.contrib import admin
 
 
-class NotDeletableAdmin(admin.ModelAdmin):
+class NotDeletableAdminMixin(object):
 
     def get_actions(self, request):
-        actions = super(NotDeletableAdmin, self).get_actions(request)
+        actions = super(NotDeletableAdminMixin, self).get_actions(request)
         del actions['delete_selected']
         return actions
 
     def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class NotAddableAdminMixin(object):
+
+    def has_add_permission(self, request):
         return False
